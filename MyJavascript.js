8a2f8e21de9f52350d56e11b5defc2c830dd1c02
Larry Jones <larryj@blinds.com>
2017-02-10 21:06:55 -0600

    Additional `console` methods:
    * `warn`
    * `error`
    * `debug`
    * `info`
    * `assert`
    
    Each of these can be filtered (`assert` logs an `error`).

--- MyJavascript.js
+++ MyJavascript.js
@@ -1,3 +1,9 @@
 doStuff = function() {
     console.log("hi");
+    console.warn("uh oh");
+    console.error("it broke!");
+    console.debug("Squish, squish"); // Debug output
+    console.info("Informative, isn't it"); // information
+    console.assert(10 == 2 * 5); // an assert passes
+    console.assert(2 + 2 == 5); // failing assertion logs an error
 }
